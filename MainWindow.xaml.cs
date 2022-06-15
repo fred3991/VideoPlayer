3310645e6827dd160c194ed9022d0032e13e1f93
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2022-06-15 14:39:39 +0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VideoPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Unosquare.FFME.Library.FFmpegDirectory = @"C:\ffmpeg\x64";
+
+          
+            MediaPrimary = new Unosquare.FFME.MediaElement();
+            MediaSecondary = new Unosquare.FFME.MediaElement();
+           
+
+            InitializeComponent();
+
+            LoadingAsync();
+        }
+        async Task LoadingAsync()
+        {
+            string p = @"C:\Repozitories\Thor.mp4";
+
+            Uri path = new Uri(p);
+
+            await MediaPrimary.Open(path);
+
+
+            string pw = @"C:\Repozitories\video.mp4";
+
+            Uri pathw = new Uri(pw);
+
+            await MediaSecondary.Open(pathw);
+        }
+
+
+
+    }
+}
